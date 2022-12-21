bb8df88c7cd901252c469b5f93fd45e38b5ee2ca
SafieDeme9 <safiedeme9@gmail.com>
2022-12-20 19:03:10 -0500

    task 1 1st attempt

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create class Square."""
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
