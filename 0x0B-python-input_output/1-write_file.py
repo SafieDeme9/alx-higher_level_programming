dca6e6e0c2e40d8dab7bac0d8c3e30e356b68faf
SafieDeme9 <safiedeme9@gmail.com>
2023-01-10 16:16:29 -0500

    task1 2nd attempt

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters written"""
-    with open(filename,mode='w', encoding="utf-8") as f:
-        number_of_lines= f.write(text)
+    """writes a string to a text file (UTF8)
+    and returns the number of characters written
+    """
+    with open(filename, mode='w', encoding="utf-8") as f:
+        number_of_lines = f.write(text)
         return number_of_lines
