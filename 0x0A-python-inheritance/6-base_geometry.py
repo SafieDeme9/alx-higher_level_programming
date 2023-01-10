caaf910518eb96619c728e792009df88ce9029cb
SafieDeme9 <safiedeme9@gmail.com>
2023-01-09 19:49:27 -0500

    task6 1st attempt

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
