ffcb18bb380a3d33a85ff750b1015adb99fca8f9
SafieDeme9 <safiedeme9@gmail.com>
2023-03-13 18:16:54 -0400

    task 2 corrected

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const len = argv.length;
+const len = process.argv.length;
 
-if (len === 3) {
-  console.log('Argument found');
-} else if (len > 3) {
-  console.log('Arguments found');
-} else {
+if (len === 2) {
   console.log('No argument found');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
