2586d77a50a8eb90fe12576be0c8be9352b5188b
SafieDeme9 <safiedeme9@gmail.com>
2023-03-13 21:14:25 -0400

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const integers = process.argv.slice(2);
+let firstMax, secondMax;
+if (integers.length > 1) {
+  for (let i = 0; i < integers.length; i++) {
+    if (i === 0) {
+      firstMax = parseInt(integers[i]);
+      secondMax = firstMax;
+    } else {
+      if (parseInt(integers[i]) > firstMax) {
+        firstMax = parseInt(integers[i]);
+      }
+      if (parseInt(integers[i]) > secondMax && parseInt(integers[i]) !== firstMax) {
+        secondMax = parseInt(integers[i]);
+      }
+    }
+  }
+  console.log(secondMax);
+} else {
+  console.log(0);
+}
