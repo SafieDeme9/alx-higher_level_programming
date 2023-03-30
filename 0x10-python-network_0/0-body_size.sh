0aa41b879ba9b32d634b5829e7d6ec1dce55fce2
SafieDeme9 <safiedeme9@gmail.com>
2023-03-30 19:52:59 -0400

    task 0 corrected

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Script that takes an URL and send request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i Content_Length | cut -d " " -f2
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+
