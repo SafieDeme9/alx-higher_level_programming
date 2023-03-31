8d16d077e0b85ce4565c04300cb85f7ffa570821
Safietou Deme <safiedeme9@gmail.com>
2023-03-31 15:46:22 -0700

    task 0 added comment and if main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reponse :
-    body = reponse.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+"""
+Python script that fetches and display body response
+"""
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reponse :
+        body = reponse.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
