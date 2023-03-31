4ce4535d2ebce401a0f80426b2e04491ee72ee6c
Safietou Deme <safiedeme9@gmail.com>
2023-03-31 15:48:31 -0700

    task 0 fixed coding style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,11 @@
 """
 Python script that fetches and display body response
 """
+
+
 if __name__ == "__main__":
     import urllib.request
+    
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as reponse :
         body = reponse.read()
         print("Body response:")
