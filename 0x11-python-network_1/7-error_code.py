9d756ec2045aa74b279231fa79878a0c25d2f508
Safietou Deme <safiedeme9@gmail.com>
2023-03-31 18:41:14 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the body of the response.
+"""
+
+
+import sys 
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if (r.status_code == requests.codes.ok):
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
