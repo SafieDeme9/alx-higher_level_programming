dbd0e0eafd6c0aba8873eb4819ab420285c7d1ae
Safietou Deme <safiedeme9@gmail.com>
2023-03-31 18:46:52 -0700

    task 7 fixed pycodestyle

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the body of the response.
+takes in a URL, sends a request to the URL
+and displays the body of the response.
 """
 
 
-import sys 
+import sys
 import requests
 
 if __name__ == "__main__":
