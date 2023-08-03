02310bef4ea670d728160014b3e907f7760898bf
SafieDeme9 <safiedeme9@gmail.com>
2023-08-02 20:48:51 -0400

    jquery tasks

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+// changed the url because of redirection errors
+$('document').ready(function () {
+    const url = 'https://hellosalut.stefanbohacek.dev/?';
+    $('INPUT#btn_translate').click(function () {
+        $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+            $('DIV#hello').html(data.hello);
+        });
+    });
+});
