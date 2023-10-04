f6a643b81b3b9edd56dabb8c5d9d11214ca89a31
SafieDeme9 <safiedeme9@gmail.com>
2023-10-04 18:06:15 -0400

    jquery tasks

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+// changed the url because i was having 301 error
+$('document').ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
